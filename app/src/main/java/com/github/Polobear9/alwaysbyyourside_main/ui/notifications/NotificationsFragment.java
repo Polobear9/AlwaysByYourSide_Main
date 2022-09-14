7682f65216984a61aa7dc79da1161e61ee1208ad
Polobear9 <lieams1996@gmail.com>
2022-09-15 00:44:22 +0900

    Make a Fragment and Set the Fragment for App, and select the back ground.

--- app/src/main/java/com/github/Polobear9/alwaysbyyourside_main/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/github/Polobear9/alwaysbyyourside_main/ui/notifications/NotificationsFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.github.Polobear9.alwaysbyyourside_main.databinding.FragmentDashboardBinding;
 import com.github.Polobear9.alwaysbyyourside_main.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
@@ -18,14 +19,12 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
+        NotificationsViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
