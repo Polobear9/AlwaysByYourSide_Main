0d4f230a6ce9caa47c76c6928238257ab690df46
Polobear9 <lieams1996@gmail.com>
2022-09-15 17:18:49 +0900

    edit the code for bluetooth code. need to edit.

--- app/src/main/java/com/github/Polobear9/alwaysbyyourside_main/MainActivity.java
+++ app/src/main/java/com/github/Polobear9/alwaysbyyourside_main/MainActivity.java
@@ -1,7 +1,14 @@
 package com.github.Polobear9.alwaysbyyourside_main;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +17,7 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,11 +27,15 @@ import com.github.Polobear9.alwaysbyyourside_main.databinding.ActivityMainBindin
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int REQUEST_ENABLE_BT = 1;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -40,16 +52,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button navigation_home_bt = findViewById(R.id.bt_home);
         navigation_home_bt.setOnClickListener(this);
-
-
     }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra((BluetoothDevice.EXTRA_DEVICE));
+                @SuppressLint("MissingPermission") String deviceName = device.getName();
+                String deviceHardWareAddress = device.getAddress(); //
+            }
+        }
+    };
 
     @Override
     public void onClick(View view) {
-        BluetoothAdapter.getDefaultAdapter();
-        Intent intent_goHome = new Intent(Intent.ACTION_WEB_SEARCH);
-        startActivity(intent_goHome);
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            System.out.println("this device can not use a bluetooth check again.");
+        } else {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+        bluetoothAdapter.startDiscovery();
+        Intent startBt = new Intent();
         Toast.makeText(this, "start the internet", Toast.LENGTH_SHORT).show();
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
 
     }
 }
